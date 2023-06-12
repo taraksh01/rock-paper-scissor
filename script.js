624d0b428132c76ea26e4e29b84ab8623c8198ee
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-12 18:21:33 +0530

    Created playRound function that plays a single round of game and return the result

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const choices = ['rock', 'paper', 'scissor'];
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
 
-function randomNumber(num){
-    return Math.floor(Math.random()*num);
+function randomNumber(num) {
+    return Math.floor(Math.random() * num);
 }
 
-console.log(getComputerChoice());
-
-function getComputerChoice(){
+function getComputerChoice() {
     return choices[randomNumber(choices.length)];
 }
 
-// console.log("File is working correctly");
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return "You Won! Rock breaks Scissor";
+    } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        return "You Lose! Scissor cuts Paper";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "You Won! Paper beats Rock";
+    } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        return "You Lose! Rock breaks Scissor";
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return "You Won! Scissor cuts Paper";
+    } else {
+        return `Match Draw! You both selected ${playerSelection}`;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
